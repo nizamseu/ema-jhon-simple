c473578fe74a48ade8c11137b9e417b4475e7f6b
nizamseu <nizamcse.seu@gmail.com>
2021-09-25 06:30:38 +0600

    search box added

--- src/components/ShopCart/ShopCart.js
+++ src/components/ShopCart/ShopCart.js
@@ -6,10 +6,14 @@ import "./ShopCart.css";
 const ShopCart = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
+  const [matchProducts, setMatchProducts] = useState([]);
   useEffect(() => {
     fetch("./products.JSON")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) => {
+        setProducts(data);
+        setMatchProducts(data);
+      });
   }, []);
 
   // add products to cart
@@ -17,16 +21,28 @@ const ShopCart = () => {
     setCart([...cart, product]);
   };
 
+  const handleChange = (e) => {
+    const inputValue = e.target.value;
+    console.log(inputValue);
+    const matched = products.filter((item) => item.name.toLowerCase().includes(inputValue.toLowerCase()));
+    setMatchProducts(matched);
+  };
+
   return (
-    <div className="shop">
-      <div className="product-container">
-        <h1>Products:{products.length}</h1>
-        {products.map((product) => (
-          <Products key={product.key} product={product} handleAdd={handleAdd}></Products>
-        ))}
+    <div>
+      <div className="search">
+        <input onChange={handleChange} type="text" placeholder="Search Product" />
       </div>
-      <div className="cart-container">
-        <Cart cart={cart}></Cart>
+      <div className="shop">
+        <div className="product-container">
+          <h1>Products:{matchProducts.length}</h1>
+          {matchProducts.map((product) => (
+            <Products key={product.key} product={product} handleAdd={handleAdd}></Products>
+          ))}
+        </div>
+        <div className="cart-container">
+          <Cart cart={cart}></Cart>
+        </div>
       </div>
     </div>
   );
