974e26408fe2e5726c729c159c3034bad7c626f3
nizamseu <nizamcse.seu@gmail.com>
2021-09-25 20:26:23 +0600

    border added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Rating from "react-rating";
 import "./Products.css";
+
 const Products = (props) => {
   const handleAdd = props.handleAdd;
   const { name, img, stock, price, seller, star } = props.product;
+
   return (
     <div className="products">
       <img src={img} alt="" />
